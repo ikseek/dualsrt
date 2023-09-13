b7cc66bbc262045ec8a297b8969b4d5d81ff7257
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2023-09-13 00:45:50 -0700

    Can specify output language on command line now

--- dualsrt/cli.py
+++ dualsrt/cli.py
@@ -4,7 +4,7 @@ from .mux import dual_subtitles
 from .extract import find_subtitles, extract_subtitle_tracks
 from itertools import product
 from re import sub
-from argparse import ArgumentParser, ArgumentTypeError, ArgumentDefaultsHelpFormatter
+from argparse import ArgumentParser, ArgumentTypeError
 
 
 def existing_file_path(path_str):
@@ -29,6 +29,7 @@ def produce_dual_subtitles(
     video: Path,
     primary_lang: str,
     secondary_lang: str,
+    output_language: str,
     primary_font: dict,
     secondary_font: dict,
 ):
@@ -44,15 +45,14 @@ def produce_dual_subtitles(
         t1 = primary["tags"].get("title") or primary["tags"]["language"]
         t2 = secondary["tags"].get("title") or secondary["tags"]["language"]
         sfx = "_".join(sub(r"[^0-9a-z]+", "_", t.lower()) for t in (t1, t2))
-        dual_file = video.parent / f"{video.stem}.{primary_lang}.{sfx}.srt"
+        dual_file = (
+            video.parent / f"{video.stem}.{output_language or primary_lang}.{sfx}.srt"
+        )
         dual_file.write_text(srt.compose(dual))
 
 
 def main():
-    parser = ArgumentParser(
-        description="Subtitle extraction and combining tool",
-        formatter_class=ArgumentDefaultsHelpFormatter,
-    )
+    parser = ArgumentParser(description="Subtitle extraction and combining tool")
     parser.add_argument(
         "primary_language",
         help="primary subtitle stream language (the one you learn)",
@@ -71,13 +71,17 @@ def main():
         "--primary-font",
         type=font_attributes,
         default="size:18",
-        help="font attirubtes of primary titles",
+        help='font attirubtes of primary titles (default: "size:18")',
     )
     parser.add_argument(
         "--secondary-font",
         type=font_attributes,
         default="size:15,color:gray",
-        help="font attributes of secondary titles",
+        help='font attributes of secondary titles (default: "size:15,color:gray")',
+    )
+    parser.add_argument(
+        "--output-language",
+        help="combined subtitle language (default is same as primary)",
     )
     args = parser.parse_args()
     for video_file in args.video_file:
@@ -85,6 +89,7 @@ def main():
             video_file,
             args.primary_language,
             args.secondary_language,
+            args.output_language,
             args.primary_font,
             args.secondary_font,
         )
